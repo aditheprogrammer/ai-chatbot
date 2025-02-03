b10542ac09a5ef1925d2a7e4c2856f4ddca5fb3e
Aditya Subedi <adityagamingsubedisus@gmail.com>
2025-02-03 12:21:28 +0545

    added api key authication

--- api.py
+++ api.py
@@ -6,13 +6,22 @@ from chat_filtered import Chat
 app = Flask(__name__)
 CORS(app)
 chat = Chat()
+API_KEYS = {"user1": "hi", "user2": "bye"}
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+def verify_api_key():
+    api_key = request.headers.get("X-API-Key")
+    if api_key not in API_KEYS.values():
+        return jsonify({"error": "Invalid API Key"}), 401
+
 @app.route("/chat", methods=["POST"])
 def chat_response():
+  auth = verify_api_key()
+  if auth:
+      return auth  # Return 401 if unauthorized
   data = request.get_json()
 
   r = chat.ans(data["query"])
